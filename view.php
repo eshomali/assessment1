357d817f2559fed4f3369a75be9660147c99c0d4
Finessemeister <eshomaly@gmail.com>
2022-08-10 23:15:35 -0400

    Update view.php

--- view.php
+++ view.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost'); 
 define('DB_NAME', 'helloworld'); 
 define('DB_USER','root'); 
-define('DB_PASSWORD','Scientia01!'); 
+define('DB_PASSWORD',''); 
 
 $con = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 
